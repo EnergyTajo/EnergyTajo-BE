af351ea826aacd2ce88e19dd4d1c7af047a2becc
b-sujin <sjsw123456@gmail.com>
2024-08-27 19:05:35 +0900

    🍎 Chore: 프로젝트 초기 설정

---
+++ src/main/java/com/energy/tajo/BitApplication.java
@@ -0,0 +1,12 @@
+package com.energy.tajo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BitApplication.class, args);
+    }
+}
