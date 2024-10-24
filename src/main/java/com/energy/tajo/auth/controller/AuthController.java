bb42166ed0cdd048cf68a258266fe441b8c2873c
b-sujin <sjsw123456@gmail.com>
2024-10-24 17:25:31 +0900

    ✨ Feat: 로그아웃 기능 업그레이드
    - 기존 : 응답만 반환
    - 현재 : 토큰 만료 시킴

--- src/main/java/com/energy/tajo/auth/controller/AuthController.java
+++ src/main/java/com/energy/tajo/auth/controller/AuthController.java
@@ -12,13 +12,16 @@ import com.energy.tajo.user.domain.User;
 import com.energy.tajo.user.dto.request.UserCreateRequest;
 import com.energy.tajo.user.repository.UserRepository;
 import com.energy.tajo.user.service.UserService;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -87,9 +90,11 @@ public class AuthController {
 
     // 로그아웃
     @PostMapping("/logout")
-    public ResponseEntity<Void> logout() {
-        return ResponseEntity.noContent().build();
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void logout(HttpServletResponse response) {
+        TokenResponse.expireAccessToken(response);
     }
+
 
     // 토큰 갱신
     @PostMapping("/refresh")
--- src/main/java/com/energy/tajo/auth/dto/response/TokenResponse.java
+++ src/main/java/com/energy/tajo/auth/dto/response/TokenResponse.java
@@ -1,5 +1,18 @@
 package com.energy.tajo.auth.dto.response;
 
-public record TokenResponse(String accessToken, String refreshToken) {
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseCookie;
 
+public record TokenResponse(String accessToken, String refreshToken) {
+    public static void expireAccessToken(HttpServletResponse response) {
+        ResponseCookie accessTokenCookie = ResponseCookie.from(HttpHeaders.AUTHORIZATION, "")
+            .httpOnly(true)
+            .secure(true)
+            .path("/")
+            .maxAge(0)
+            .build();
+
+        response.addHeader(HttpHeaders.SET_COOKIE, accessTokenCookie.toString());
+    }
 }
