a1d9694c7bd4630aa111eb7dd9268ac82a9e6e9e
SUJIN <67819075+b-sujin@users.noreply.github.com>
2024-10-30 18:40:46 +0900

    Update WebConfig.java
    CORS 설정 추가

--- src/main/java/com/energy/tajo/config/WebConfig.java
+++ src/main/java/com/energy/tajo/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("https://www.energytajo.site", "http://localhost:3000")
+            .allowedOrigins("https://www.energytajo.site", "http://localhost:3000", "http://192.168.45.106:3000")
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
             .allowCredentials(true);
     }
